334c550dc476def85f23b05585ca131e0f5919b7
Diana Seo <syb0127@berkeley.edu>
2024-01-04 00:22:36 +0900

    Style: 판매 중 뱃지 퍼블리싱 작업

--- src/components/room-for-sale/index.tsx
+++ src/components/room-for-sale/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './roomForSale.scss';
 
 const RoomForSale = () => {
---
+++ src/pages/room/index.tsx
@@ -0,0 +1,12 @@
+import './roomForSale.scss';
+import RoomForSale from '../../components/room-for-sale/index';
+
+const Room = () => {
+  return (
+    <>
+      <RoomForSale />
+    </>
+  );
+};
+
+export default Room;
