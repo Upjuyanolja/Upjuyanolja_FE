bf9650cddd79eb1b2029c356ebdf2cc5c9fde373
xxxjinn <tlsguswls319@naver.com>
2024-01-03 16:31:29 +0900

    Feat: layout.tsx 추가

---
+++ src/layout.tsx
@@ -0,0 +1,19 @@
+import { Layout } from 'antd';
+import { Outlet } from 'react-router-dom';
+
+export const RootLayout = () => {
+  return (
+    <Layout
+      style={{
+        maxWidth: '1280px',
+        minHeight: '100vh',
+        margin: '0 auto',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Outlet />
+    </Layout>
+  );
+};
