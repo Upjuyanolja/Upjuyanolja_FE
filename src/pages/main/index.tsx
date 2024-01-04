fd4770ec86ee6888ea3c41d791d027e1d7160ad6
89882 <pgh123166@naver.com>
2024-01-04 11:19:59 +0900

    Feat: styled components install

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,3 +1,21 @@
+import { Button, Modal } from 'antd';
+import styled from 'styled-components';
+
 export const Main = () => {
-  return <div>main page</div>;
+  return (
+    <>
+      <StyledModal title="Basic Modal" open={false}>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </StyledModal>
+      <Button type="primary">가나다라</Button>
+    </>
+  );
 };
+
+const StyledModal = styled(Modal)`
+  .ant-modal-header {
+    height: 400px;
+  }
+`;
