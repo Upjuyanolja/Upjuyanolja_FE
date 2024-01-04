6ad14a9b93e9b16eddebaa7295dd465843b88528
xxxjinn <tlsguswls319@naver.com>
2024-01-04 12:15:54 +0900

    Style: styled-components 적용으로 인한 스타일 수정

--- src/pages/init/index.tsx
+++ src/pages/init/index.tsx
@@ -1,53 +1,61 @@
 import { Layout, Typography, Button } from 'antd';
 import { MdOutlineAddHome } from 'react-icons/md';
-import './init.module.scss';
+import styled from 'styled-components';
 
 export const Init = () => {
   const { Title } = Typography;
   const name = '신현진';
 
   return (
-    <Layout
-      style={{
-        height: '100vh',
-        backgroundColor: 'white',
-      }}
-    >
+    <StyledLayout>
       <Layout.Header>Header</Layout.Header>
-      <Layout.Content
-        style={{
-          width: '1024px',
-          margin: '0 auto',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          gap: '10px',
-        }}
-      >
-        <div className="main-content">
+      <StyledContent>
+        <MainContent>
+          {/*antd 커스터마이징 후 수정 */}
           <Title level={2} style={{ fontSize: '36px' }}>
             반갑습니다, {name}님!
           </Title>
-          <Button
+          <StyledButton
             ghost
             type="primary"
-            style={{
-              width: '784px',
-              height: '72px',
-              fontSize: '24px',
-              fontWeight: 'bold',
-              display: 'flex',
-              alignItems: 'center',
-              gap: '10px',
-            }}
             href="/init/accommodation-registration"
           >
-            <MdOutlineAddHome size={32} />
+            <MdOutlineAddHome />
             숙소 등록 시작하기
-          </Button>
-        </div>
-      </Layout.Content>
-    </Layout>
+          </StyledButton>
+        </MainContent>
+      </StyledContent>
+    </StyledLayout>
   );
 };
+
+const StyledLayout = styled(Layout)`
+  height: 100vh;
+  background-color: white;
+`;
+
+const StyledContent = styled(Layout.Content)`
+  width: 102.4rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+`;
+
+const MainContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: left;
+`;
+
+const StyledButton = styled(Button)`
+  width: 78.4rem;
+  height: 7.2rem;
+  font-size: 2.4rem;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
