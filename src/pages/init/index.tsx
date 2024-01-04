d19e97b8aa88e75dd159914a21957618b027b917
xxxjinn <tlsguswls319@naver.com>
2024-01-04 10:26:59 +0900

    Style: 스타일 수정

--- src/pages/init/index.tsx
+++ src/pages/init/index.tsx
@@ -1,6 +1,7 @@
 import { Layout, Typography, Button } from 'antd';
-import { Content, Header } from 'antd/lib/layout/layout';
+import { Content } from 'antd/lib/layout/layout';
 import { MdOutlineAddHome } from 'react-icons/md';
+import './init.module.scss';
 
 export const Init = () => {
   const { Title } = Typography;
@@ -10,47 +11,43 @@ export const Init = () => {
     <Layout
       style={{
         height: '100vh',
+        backgroundColor: 'white',
       }}
     >
-      <Header
-        style={{
-          width: '100vw',
-          height: '4vh',
-          backgroundColor: 'lightblue',
-        }}
-      >
-        Header
-      </Header>
+      <Layout.Header>Header</Layout.Header>
       <Content
         style={{
-          maxWidth: '1024px',
+          width: '1024px',
           margin: '0 auto',
           display: 'flex',
           flexDirection: 'column',
           justifyContent: 'center',
+          alignItems: 'center',
           gap: '10px',
         }}
       >
-        <Title level={2} style={{ fontSize: '36px' }}>
-          반갑습니다, {name}님!
-        </Title>
-        <Button
-          ghost
-          type="primary"
-          style={{
-            width: '784px',
-            height: '72px',
-            fontSize: '24px',
-            fontWeight: 'bold',
-            display: 'flex',
-            alignItems: 'center',
-            gap: '10px',
-          }}
-          href="/init/accommodation-registration"
-        >
-          <MdOutlineAddHome size={32} />
-          숙소 등록 시작하기
-        </Button>
+        <div className="main-content">
+          <Title level={2} style={{ fontSize: '36px' }}>
+            반갑습니다, {name}님!
+          </Title>
+          <Button
+            ghost
+            type="primary"
+            style={{
+              width: '784px',
+              height: '72px',
+              fontSize: '24px',
+              fontWeight: 'bold',
+              display: 'flex',
+              alignItems: 'center',
+              gap: '10px',
+            }}
+            href="/init/accommodation-registration"
+          >
+            <MdOutlineAddHome size={32} />
+            숙소 등록 시작하기
+          </Button>
+        </div>
       </Content>
     </Layout>
   );
