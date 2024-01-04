c7fefcb3dc1ebd36a0fa242109c9c09d39f6c19d
Taegwan Youn <tkyoun0421@naver.com>
2024-01-04 20:34:13 +0900

    Style: cursor default 설정

--- src/components/text/type.ts
+++ src/components/text/type.ts
@@ -6,42 +6,52 @@ export const typographyMap = {
   h1: css`
     font-size: 40px;
     line-height: 1.5;
+    cursor: default;
   `,
   h2: css`
     font-size: 36px;
     line-height: 1.5;
+    cursor: default;
   `,
   h3: css`
     font-size: 32px;
     line-height: 1.5;
+    cursor: default;
   `,
   h4: css`
     font-size: 24px;
     line-height: 1.5;
+    cursor: default;
   `,
   h5: css`
     font-size: 20px;
     line-height: 1.5;
+    cursor: default;
   `,
   body1: css`
     font-size: 18px;
     line-height: 1.5;
+    cursor: default;
   `,
   body2: css`
     font-size: 16px;
     line-height: 1.5;
+    cursor: default;
   `,
   body3: css`
     font-size: 14px;
     line-height: 1.5;
+    cursor: default;
   `,
   body4: css`
     font-size: 12px;
     line-height: 1.5;
+    cursor: default;
   `,
   body5: css`
     font-size: 10px;
     line-height: 1.5;
+    cursor: default;
   `,
 };
 
