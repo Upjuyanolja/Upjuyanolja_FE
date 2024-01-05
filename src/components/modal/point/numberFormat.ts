2a75adfd52de4d2feaa3ff141dfca90ef85984a0
YongYong21 <imkinggosu@gmail.com>
2024-01-05 17:57:51 +0900

    Refactor: numberFormat 수정

--- src/components/modal/point/numberFormat.ts
+++ src/components/modal/point/numberFormat.ts
@@ -1,10 +1,6 @@
 export const numberFormat = (input: string): string => {
-  if (input) {
-    const numericValue = parseInt(input.replace(/[^\d]/g, ''));
-    return numericValue.toLocaleString();
-  } else {
-    return '';
-  }
+  const numericValue = parseInt(input.replace(/[^\d]/g, ''));
+  return numericValue.toLocaleString();
 };
 
 export const removeNumberFormat = (input: string): string => {
