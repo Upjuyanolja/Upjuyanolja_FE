2c13a888875d7713f723065f368eae401fad4242
Taegwan Youn <tkyoun0421@naver.com>
2024-01-05 16:12:54 +0900

    Style: 쿠폰 타입 선택 완성

---
+++ src/components/coupon-registration/coupon-card/type.ts
@@ -0,0 +1,12 @@
+export type ContainerProps = {
+  $borderColor?: string;
+};
+
+export type TitleProps = {
+  $bgColor?: string;
+};
+
+export type CouponCardProps = {
+  title: string;
+  children: React.ReactNode;
+};
--- src/pages/coupon-registration/index.tsx
+++ src/pages/coupon-registration/index.tsx
@@ -1,3 +1,21 @@
+import { CouponCard } from '@components/coupon-registration/coupon-card';
+import { CouponType } from '@components/coupon-registration/coupon-type';
+import { TextBox } from '@components/text-box';
+import styled from 'styled-components';
+
 export const CouponRegistration = () => {
-  return <div>coupon registration page</div>;
+  return (
+    <Container>
+      <TextBox typography="h4" fontWeight="bold">
+        쿠폰 만들기
+      </TextBox>
+      <CouponCard title="1. 쿠폰 유형 선택">
+        <CouponType />
+      </CouponCard>
+    </Container>
+  );
 };
+
+const Container = styled.section`
+  margin: 32px 0 0 48px;
+`;
