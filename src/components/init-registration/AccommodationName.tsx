8520784fe627f092484dab1a2e861d1797b29e70
xxxjinn <tlsguswls319@naver.com>
2024-01-05 20:33:15 +0900

    Feat: 숙소명 컴포넌트 글자 수 세기 ant design 제공 props 사용

--- src/components/init-registration/AccommodationName.tsx
+++ src/components/init-registration/AccommodationName.tsx
@@ -2,19 +2,10 @@ import { useState, ChangeEvent } from 'react';
 import { styled } from 'styled-components';
 import { FormErrorMessage } from './FormErrorMessage';
 import { Input, Form } from 'antd';
-import { TextBox } from '@components/text-box';
 
 export const AccommodationName = () => {
   const [inputValue, setInputValue] = useState('');
-  const [characterCount, setCharacterCount] = useState(0);
   const [error, setError] = useState<string | null>(null);
-
-  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const newValue = event.target.value.slice(0, 30);
-    setInputValue(newValue);
-    setCharacterCount(newValue.length);
-    validateInput(newValue);
-  };
 
   const validateInput = (value: string) => {
     if (value.length < 2) {
@@ -24,24 +15,29 @@ export const AccommodationName = () => {
     }
   };
 
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const newValue = event.target.value.slice(0, 30);
+    setInputValue(newValue);
+    validateInput(newValue);
+  };
+
   return (
     <StyledInputWrapper>
       <Form.Item rules={[{ required: true }]} label="숙소명" colon={false}>
-        <StyledInput
+        <Input
           id="accommodationName"
           placeholder="숙소명을 입력해 주세요."
           type="text"
           minLength={2}
           maxLength={30}
+          showCount
+          style={{ height: 40 }}
           value={inputValue}
           onChange={handleInputChange}
           disabled={inputValue.length >= 30}
         />
         <StyledCharacterCountWrapper>
           {error && <StyledFormErrorMessage errorMessage={error} />}
-          <StyledTextBox typography="body3" color="black500">
-            {characterCount}/30
-          </StyledTextBox>
         </StyledCharacterCountWrapper>
       </Form.Item>
     </StyledInputWrapper>
@@ -71,20 +67,12 @@ const StyledInputWrapper = styled.div`
   }
 `;
 
-const StyledInput = styled(Input)`
-  height: 40px;
-`;
-
 const StyledCharacterCountWrapper = styled.div`
   height: 18px;
 `;
 
 const StyledFormErrorMessage = styled(FormErrorMessage)`
   float: left;
-`;
-
-const StyledTextBox = styled(TextBox)`
-  float: right;
 `;
 
 export default AccommodationName;
