aee277a8438ee199685efcd9bbc1447371de8ba7
Taegwan Youn <tkyoun0421@naver.com>
2024-01-05 17:44:47 +0900

    Remove: 안쓰는 type 제거

--- src/components/coupon-registration/coupon-card/index.tsx
+++ src/components/coupon-registration/coupon-card/index.tsx
@@ -1,7 +1,7 @@
 import { colors } from '@/constants/colors';
 import { TextBox } from '@components/text-box';
 import styled from 'styled-components';
-import { ContainerProps, CouponCardProps, TitleProps } from './type';
+import { CouponCardProps } from './type';
 
 export const CouponCard = ({ title, children }: CouponCardProps) => {
   return (
@@ -16,14 +16,14 @@ export const CouponCard = ({ title, children }: CouponCardProps) => {
   );
 };
 
-const Container = styled.div<ContainerProps>`
+const Container = styled.div`
   border: 2px solid ${colors.primary};
   border-radius: 8px;
   overflow: hidden;
   box-shadow: 0px 1px 5px 0px #0000001a;
 `;
 
-const Title = styled.div<TitleProps>`
+const Title = styled.div`
   padding: 8px 0 8px 8px;
   background-color: ${colors.light};
 `;
--- src/components/coupon-registration/coupon-card/type.ts
+++ src/components/coupon-registration/coupon-card/type.ts
@@ -1,11 +1,3 @@
-export type ContainerProps = {
-  $borderColor?: string;
-};
-
-export type TitleProps = {
-  $bgColor?: string;
-};
-
 export type CouponCardProps = {
   title: string;
   children: React.ReactNode;
