9f392f7f018c073c63c97fff134f52550d53c4d3
yangjaehyuk <gndksakxk10@naver.com>
2024-01-05 16:35:06 +0900

    Test: 서비스 이용 동의 페이지 테스트 코드 구현

--- src/test/sign-in-agreement/SignInAgreement.test.tsx
+++ src/test/sign-in-agreement/SignInAgreement.test.tsx
@@ -4,6 +4,7 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import { SignInAgreement } from '.';
 import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
 
 const mockedUsedNavigate = jest.fn();
 
@@ -12,15 +13,82 @@ jest.mock('react-router-dom', () => ({
   useNavigate: () => mockedUsedNavigate,
 }));
 
-// test('모두 동의 버튼 클릭 테스트', () => {});
+test('모두 동의 버튼 클릭 테스트', async () => {
+  // 1. Arrange
+  render(<SignInAgreement />);
+  for (let i = 0; i < 4; i++) {
+    const checkId = await screen.getByRole('checkbox', { name: `${i}` });
+    expect(checkId).not.toBeChecked();
+  }
+  const allCheck = await screen.getByRole('checkbox', { name: '모두 동의' });
+  expect(allCheck).not.toBeChecked();
 
-// test('필수 버튼 클릭 테스트', () => {});
+  // 2. Act
+  fireEvent.click(allCheck);
+  expect(allCheck).toBeChecked();
 
-// test('선택 버튼 클릭 테스트', () => {});
+  // 3. Assert
+  for (let i = 0; i < 4; i++) {
+    const checkId = await screen.getByRole('checkbox', { name: `${i}` });
+    expect(checkId).toBeChecked();
+  }
+  const nextBtn = await screen.findByTestId('nextBtn');
+  expect(nextBtn.textContent).toBe('다음');
+  expect(nextBtn).not.toBeDisabled();
+});
+
+test('필수 버튼 클릭 테스트', async () => {
+  // 1. Arrange
+  render(<SignInAgreement />);
+  for (let i = 0; i < 4; i++) {
+    const checkId = await screen.getByRole('checkbox', { name: `${i}` });
+    expect(checkId).not.toBeChecked();
+  }
+  const allCheck = await screen.getByRole('checkbox', { name: '모두 동의' });
+  expect(allCheck).not.toBeChecked();
+  const EssentialCheck = await screen.getByRole('checkbox', { name: '0' });
+
+  // 2. Act
+  fireEvent.click(EssentialCheck);
+  expect(EssentialCheck).toBeChecked();
+
+  // 3. Assert
+  const nextBtn = await screen.findByTestId('nextBtn');
+  expect(nextBtn).not.toBeDisabled();
+});
+
+test('선택 버튼 클릭 테스트', async () => {
+  // 1. Arrange
+  render(<SignInAgreement />);
+  for (let i = 0; i < 4; i++) {
+    const checkId = await screen.getByRole('checkbox', { name: `${i}` });
+    expect(checkId).not.toBeChecked();
+  }
+  const allCheck = await screen.getByRole('checkbox', { name: '모두 동의' });
+  expect(allCheck).not.toBeChecked();
+
+  // 2. Act
+  for (let i = 1; i < 4; i++) {
+    const selectiveCheck = await screen.getByRole('checkbox', { name: `${i}` });
+    fireEvent.click(selectiveCheck);
+    expect(selectiveCheck).toBeChecked();
+  }
+
+  // 3. Assert
+  const nextBtn = await screen.findByTestId('nextBtn');
+  expect(nextBtn.textContent).toBe('다음');
+  expect(nextBtn).toBeDisabled();
+});
 
 test('이전 버튼 클릭 테스트', async () => {
   // 1. Arrange
   render(<SignInAgreement />);
+  for (let i = 0; i < 4; i++) {
+    const checkId = await screen.getByRole('checkbox', { name: `${i}` });
+    expect(checkId).not.toBeChecked();
+  }
+  const allCheck = await screen.getByRole('checkbox', { name: '모두 동의' });
+  expect(allCheck).not.toBeChecked();
 
   // 2. Act
   const prevBtn = await screen.findByTestId('prevBtn');
@@ -33,6 +101,61 @@ test('이전 버튼 클릭 테스트', async () => {
   }, 5000);
 });
 
-// test('다음 버튼 클릭 테스트(모두 동의 버튼 눌러 활성화 된 경우)', () => {});
+test('다음 버튼 클릭 테스트(모두 동의 버튼 눌러 활성화 된 경우)', async () => {
+  // 1. Arrange
+  render(<SignInAgreement />);
+  for (let i = 0; i < 4; i++) {
+    const checkId = await screen.getByRole('checkbox', { name: `${i}` });
+    expect(checkId).not.toBeChecked();
+  }
+  const allCheck = await screen.getByRole('checkbox', { name: '모두 동의' });
+  expect(allCheck).not.toBeChecked();
 
-// test('다음 버튼 클릭 테스트(필수 버튼 눌러 활성화 된 경우)', () => {});
+  // 2. Act
+  fireEvent.click(allCheck);
+  expect(allCheck).toBeChecked();
+
+  // 3. Assert
+  for (let i = 0; i < 4; i++) {
+    const checkId = await screen.getByRole('checkbox', { name: `${i}` });
+    expect(checkId).toBeChecked();
+  }
+  const nextBtn = await screen.findByTestId('nextBtn');
+  expect(nextBtn.textContent).toBe('다음');
+  expect(nextBtn).not.toBeDisabled();
+
+  // 4. Act
+  fireEvent.click(nextBtn);
+  setTimeout(() => {
+    expect(window.location.pathname).toBe('/signup');
+  }, 5000);
+});
+
+test('다음 버튼 클릭 테스트(필수 버튼 눌러 활성화 된 경우)', async () => {
+  // 1. Arrange
+  render(<SignInAgreement />);
+  for (let i = 0; i < 4; i++) {
+    const checkId = await screen.getByRole('checkbox', { name: `${i}` });
+    expect(checkId).not.toBeChecked();
+  }
+  const allCheck = await screen.getByRole('checkbox', { name: '모두 동의' });
+  expect(allCheck).not.toBeChecked();
+
+  // 2. Act
+  for (let i = 1; i < 4; i++) {
+    const selectiveCheck = await screen.getByRole('checkbox', { name: `${i}` });
+    fireEvent.click(selectiveCheck);
+    expect(selectiveCheck).toBeChecked();
+  }
+
+  // 3. Assert
+  const nextBtn = await screen.findByTestId('nextBtn');
+  expect(nextBtn.textContent).toBe('다음');
+  expect(nextBtn).toBeDisabled();
+
+  // 4. Act
+  fireEvent.click(nextBtn);
+  setTimeout(() => {
+    expect(window.location.pathname).toBe('/signup');
+  }, 5000);
+});
