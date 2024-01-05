c03be8789ea7e84f727762f152d1f9bfbe3cdca9
Taegwan Youn <tkyoun0421@naver.com>
2024-01-05 19:50:36 +0900

    Style: 쿠폰 미리보기 퍼블리싱 완성

--- src/components/coupon-registration/coupon-preview/index.tsx
+++ src/components/coupon-registration/coupon-preview/index.tsx
@@ -2,6 +2,9 @@ import { colors } from '@/constants/colors';
 import { TextBox } from '@components/text-box';
 import styled from 'styled-components';
 import { CouponPreviewItem } from './coupoin-preview-item';
+import { Spacing } from '@components/spacing';
+import { useEffect, useState } from 'react';
+import { Button, Checkbox } from 'antd';
 
 const couponMap = {
   label: '5000원 할인 쿠폰',
@@ -22,6 +25,17 @@ const couponMap = {
 };
 
 export const CouponPreview = () => {
+  const [totalPrice, setTotalPrice] = useState<number>(0);
+
+  useEffect(() => {
+    const calculatedTotalPrice = couponMap.coupons.reduce(
+      (accumulator, currentCoupon) =>
+        accumulator + currentCoupon.couponPrice * currentCoupon.couponAmount,
+      0,
+    );
+
+    setTotalPrice(calculatedTotalPrice);
+  }, [couponMap.coupons]);
   return (
     <Container>
       <TextBox typography="h4" fontWeight="bold" color="black900">
@@ -33,11 +47,33 @@ export const CouponPreview = () => {
             {couponMap.label}
           </TextBox>
         </StyledTitleWrap>
-        <StyledCouponItemWrap>
-          {couponMap.coupons.map((item, index) => (
-            <CouponPreviewItem data={item} key={index} />
-          ))}
-        </StyledCouponItemWrap>
+        {couponMap.coupons.map((item, index) => (
+          <CouponPreviewItem data={item} key={index} />
+        ))}
+        <Spacing space="16" />
+        <StyledCouponTotalPrice>
+          <TextBox typography="h5" fontWeight="bold" color="primary">
+            합계 : {totalPrice}P
+          </TextBox>
+        </StyledCouponTotalPrice>
+        <Spacing space="16" />
+        <StyledTermsAgreement>
+          <Checkbox />
+          <TextBox typography="body4" color="black900">
+            주문 내용을 확인하였으며,{' '}
+            <TextBox typography="body4" color="primaryHover">
+              구매 약관
+            </TextBox>{' '}
+            등에 동의합니다.
+          </TextBox>
+        </StyledTermsAgreement>
+        <Spacing space="16" />
+        <StyledButton>
+          <TextBox typography="h5" fontWeight="bold" color="white">
+            구매하기
+          </TextBox>
+        </StyledButton>
+        <Spacing space="16" />
       </StyledCouponWrap>
     </Container>
   );
@@ -67,4 +103,34 @@ const StyledTitleWrap = styled.div`
   background-color: ${colors.light};
 `;
 
-const StyledCouponItemWrap = styled.div``;
+const StyledCouponTotalPrice = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  padding: 12px 16px 12px 0;
+  margin: 0 12px;
+  border: 2px solid ${colors.primary};
+  border-radius: 2px;
+  background: linear-gradient(268.34deg, #e0edff 1.74%, #ffffff 120.49%);
+`;
+
+const StyledTermsAgreement = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 7px;
+`;
+
+const StyledButton = styled(Button)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 12px;
+  background-color: ${colors.primary};
+  height: 54px;
+  &:hover {
+    background-color: ${colors.primaryHover};
+  }
+  &:active {
+    background-color: ${colors.primaryActive};
+  }
+`;
