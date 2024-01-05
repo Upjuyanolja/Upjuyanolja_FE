794f183470be01f8e9d8840a5825d0c5ff16fbd8
YongYong21 <imkinggosu@gmail.com>
2024-01-05 11:52:24 +0900

    Feat: 포인트 충전 모달 로직 구현

--- src/pages/point-detail/index.tsx
+++ src/pages/point-detail/index.tsx
@@ -4,23 +4,65 @@ import {
   Modal,
   Layout,
   Typography,
-  InputNumber,
+  // InputNumber,
   Form,
   Space,
+  Input,
 } from 'antd';
 
 import { useState } from 'react';
 import styled from 'styled-components';
+import { numberFormat, removeNumberFormat } from './numberFormat';
+import { ExclamationCircleOutlined } from '@ant-design/icons';
+// import { TextBox } from '@components/text-box';
+const MINIMUM_PRICE = 10000;
+const MAXIMUM_PRICE = 10000000;
 
 export const PointDetail = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  // const [point, setPoint] = useState<number | null>();
-  const [form] = Form.useForm<{ point: number }>();
-  // const handleChangePoint = (e: React.ChangeEvent) => {
-  //   if (e.currentTarget) {
-  //     setPoint(e.currentTarget)
-  //   }
-  // };
+  // const [point, setPoint] = useState<number>();
+  const [formattedValue, setFormattedValue] = useState<string>();
+  const [pointErrorMessage, setPointErrorMessage] = useState<string>('');
+  const [form] = Form.useForm<{ inputValue: string }>();
+
+  const [isPointState, setIsPointState] = useState<boolean>(true);
+
+  const [isAgreementPoint, setIsAgreementPoint] = useState(false);
+
+  const handleChangePoint = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value;
+    if (/[0-9]/.test(removeNumberFormat(inputValue))) {
+      setFormattedValue(numberFormat(inputValue));
+    } else {
+      setFormattedValue('');
+    }
+
+    if (
+      parseInt(removeNumberFormat(inputValue)) < MINIMUM_PRICE ||
+      inputValue === ''
+    ) {
+      setPointErrorMessage('1회 최소 충전가능한 포인트는 10,000 포인트입니다.');
+      setIsPointState(true);
+    } else if (parseInt(removeNumberFormat(inputValue)) > MAXIMUM_PRICE) {
+      setFormattedValue(numberFormat(`${MAXIMUM_PRICE}`));
+      setIsPointState(false);
+    } else {
+      setPointErrorMessage('');
+      setIsPointState(false);
+    }
+  };
+
+  const handleClickAddPoint = (price: number) => {
+    const result = parseInt(removeNumberFormat(formattedValue || '0')) + price;
+    if (result > MAXIMUM_PRICE) {
+      setFormattedValue(numberFormat(`${MAXIMUM_PRICE}`));
+    } else {
+      setFormattedValue(numberFormat(result.toString()));
+      setPointErrorMessage('');
+      setIsPointState(false);
+    }
+  };
+
   const showModal = () => {
     setIsModalOpen(true);
   };
@@ -55,26 +97,41 @@ export const PointDetail = () => {
             style={{ backgroundColor: 'white' }}
           >
             <Form.Item name="point" label="충전할 포인트">
-              <InputNumber
+              <Input
                 style={{ width: '95%' }}
-                controls={false}
                 placeholder="충전할 포인트를 입력해 주세요."
+                value={formattedValue}
+                onChange={handleChangePoint}
               />
               <span style={{ marginLeft: '8px' }}>P</span>
+              <div>
+                <span>{pointErrorMessage}</span>
+              </div>
             </Form.Item>
 
             <Space className="point-buttonWrap">
-              <Button>+ 1만</Button>
-              <Button>+ 5만</Button>
-              <Button>+ 10만</Button>
+              <Button onClick={() => handleClickAddPoint(10000)}>+ 1만</Button>
+              <Button onClick={() => handleClickAddPoint(50000)}>+ 5만</Button>
+              <Button onClick={() => handleClickAddPoint(100000)}>
+                + 10만
+              </Button>
             </Space>
 
             <Space className="price-wrap">
-              <Typography>결제 금액 : 0원</Typography>
+              {/* <InfoPriceContainer>
+                <TextBox>충전하시는 포인트에 따른 결제 금액입니다.</TextBox>
+                <TextBox>*포인트 환산 기준: 결제 금액 = 포인트</TextBox>
+              </InfoPriceContainer> */}
+              <ExclamationCircleOutlined />
+              <Typography>결제 금액 : {formattedValue}원</Typography>
             </Space>
 
             <Space className="agreement-wrap">
-              <Checkbox>
+              <Checkbox
+                onChange={() => {
+                  setIsAgreementPoint(!isAgreementPoint);
+                }}
+              >
                 <Typography>
                   주문 내용을 확인하였으며, 구매 약관 등에 동의합니다
                 </Typography>
@@ -85,6 +142,7 @@ export const PointDetail = () => {
               key="submit"
               type="primary"
               onClick={handleOk}
+              disabled={isPointState || !isAgreementPoint}
               style={{
                 width: '100%',
                 height: '46px',
@@ -169,3 +227,17 @@ const CustomModal = styled(Modal)`
     text-align: center;
   }
 `;
+
+// const InfoPriceContainer = styled(Space)`
+//   position: absolute;
+//   top: 50px;
+
+//   width: 266px;
+//   height: 56px;
+
+//   border-radius: 2px;
+
+//   padding: 6px 8px;
+
+//   box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.15);
+// `;
