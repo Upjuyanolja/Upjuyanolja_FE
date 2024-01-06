186b2b9eafdc7bf5c480c88a3cc5c73c9f965aa0
yangjaehyuk <gndksakxk10@naver.com>
2024-01-07 05:07:24 +0900

    Feat: msw handler 구현

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,1 +1,13 @@
-export const handlers = [];
+import { http, HttpResponse } from 'msw';
+import signInData from '../data/sign-in/signInData.json';
+import accomodationsData from '../data/sign-in/accomodationsData.json';
+const postSignInResolver = async () => {
+  return HttpResponse.json(signInData, { status: 400 });
+};
+const getAccomodationsResolver = () => {
+  return HttpResponse.json(accomodationsData, { status: 400 });
+};
+export const handlers = [
+  http.post('/api/auth/owner/signin', postSignInResolver),
+  http.get('/api/accommodations/backoffice', getAccomodationsResolver),
+];
