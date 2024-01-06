eb513f770b330da4b03868f9d5b576049918e952
yangjaehyuk <gndksakxk10@naver.com>
2024-01-07 04:03:15 +0900

    Setting: msw 설정

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,13 @@ import reportWebVitals from './reportWebVitals';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RecoilRoot } from 'recoil';
 import './index.css';
+import { worker } from './mocks/browsers';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 const queryClient = new QueryClient();
+worker.start();
 root.render(
   <React.StrictMode>
     <RecoilRoot>
