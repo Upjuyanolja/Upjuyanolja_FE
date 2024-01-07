1f67c36a48363c981edb50c9a2fe6a4fa5910e3e
xxxjinn <tlsguswls319@naver.com>
2024-01-07 18:07:47 +0900

    Test: 주소 api 테스트코드 작성

--- src/test/init-accommodation-registration/InitAccommodationRegistration.test.tsx
+++ src/test/init-accommodation-registration/InitAccommodationRegistration.test.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import '../matchMedia.mock';
 import { AccommodationName } from '@components/init-registration/AccommodationName';
 import { AccommodationDesc } from '@components/init-registration/AccommodationDesc';
+import { AccommodationAddress } from '@components/init-registration/AccommodationAddress';
+import axios from 'axios';
 
 describe('InitAccommodationRegistration', () => {
   test('숙소명을 2글자 미만 입력했을 때 에러메세지를 띄운다.', () => {
@@ -54,37 +56,61 @@ describe('InitAccommodationRegistration', () => {
     expect(inputAccommodationName).toHaveAttribute('disabled');
   });
 
-  test('숙소소개에 10글자 미만 입력했을 때 에러메세지를 띄운다.', () => {
+  test('주소 검색 버튼을 누르면 주소 api를 호출하고, 데이터를 state로 설정해 출력한다.', () => {
     render(
       <BrowserRouter>
-        <AccommodationDesc />
+        <AccommodationAddress />
       </BrowserRouter>,
     );
-    const testAreaAccommodationDesc = screen.getByTestId(
-      'textarea-accommodation-desc',
+    const accommodationAddressAPIButton = screen.getByTestId(
+      'acccommodation-address-api-button',
     );
-    act(() => {
-      userEvent.type(testAreaAccommodationDesc, '안녕');
-    });
-    const errorMessage = screen.getByTestId(
-      'error-textarea-accommodation-desc',
-    );
-    expect(errorMessage).toBeInTheDocument();
-  });
 
-  test('숙소소개에 500자를 초과해 입력했을 때 input을 막는다.', () => {
-    render(
-      <BrowserRouter>
-        <AccommodationDesc />
-      </BrowserRouter>,
-    );
-    const testAreaAccommodationDesc = screen.getByTestId(
-      'textarea-accommodation-desc',
-    );
+    const responseData = { post: '12345', address: '경기도 구리시' };
+    const axiosMock = jest.spyOn(axios, 'get');
+    axiosMock.mockResolvedValue({ data: responseData });
+
     act(() => {
-      userEvent.type(testAreaAccommodationDesc, 'a'.repeat(501));
+      userEvent.click(accommodationAddressAPIButton);
     });
 
-    expect(testAreaAccommodationDesc).toHaveAttribute('disabled');
+    expect(screen.getByTestId('accommodation-post')).toHaveValue(
+      responseData.post,
+    );
+    expect(screen.getByTestId('accommodation-address')).toHaveValue(
+      responseData.address,
+    );
   });
+});
+
+test('숙소소개에 10글자 미만 입력했을 때 에러메세지를 띄운다.', () => {
+  render(
+    <BrowserRouter>
+      <AccommodationDesc />
+    </BrowserRouter>,
+  );
+  const testAreaAccommodationDesc = screen.getByTestId(
+    'textarea-accommodation-desc',
+  );
+  act(() => {
+    userEvent.type(testAreaAccommodationDesc, '안녕');
+  });
+  const errorMessage = screen.getByTestId('error-textarea-accommodation-desc');
+  expect(errorMessage).toBeInTheDocument();
+});
+
+test('숙소소개에 500자를 초과해 입력했을 때 input을 막는다.', () => {
+  render(
+    <BrowserRouter>
+      <AccommodationDesc />
+    </BrowserRouter>,
+  );
+  const testAreaAccommodationDesc = screen.getByTestId(
+    'textarea-accommodation-desc',
+  );
+  act(() => {
+    userEvent.type(testAreaAccommodationDesc, 'a'.repeat(501));
+  });
+
+  expect(testAreaAccommodationDesc).toHaveAttribute('disabled');
 });
