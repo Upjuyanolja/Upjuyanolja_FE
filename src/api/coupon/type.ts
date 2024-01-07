65d55a19425612eb837f7407c31e8c2573933f12
89882 <pgh123166@naver.com>
2024-01-07 13:26:55 +0900

    Feat: getRevenue api 함수 구현

--- src/api/coupon/type.ts
+++ src/api/coupon/type.ts
@@ -3,3 +3,12 @@ export type staticsData = {
   used: number;
   stock: number;
 };
+
+export type dailyRevenue = {
+  day: string;
+  couponRevenue: number;
+  normalRevenue: number;
+};
+export type revenueData = {
+  revenue: dailyRevenue[];
+};
