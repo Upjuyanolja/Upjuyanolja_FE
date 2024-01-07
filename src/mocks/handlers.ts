063268eb3cafe1f4e061d2ea127c8a4d141aaaaf
yangjaehyuk <gndksakxk10@naver.com>
2024-01-07 14:56:57 +0900

    Chore: msw handler 변경

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,12 +1,6 @@
-import { http, HttpResponse } from 'msw';
-import signInData from '../data/sign-in/signInData.json';
-import accomodationsData from '../data/sign-in/accomodationsData.json';
-const postSignInResolver = async () => {
-  return HttpResponse.json(signInData, { status: 400 });
-};
-const getAccomodationsResolver = () => {
-  return HttpResponse.json(accomodationsData, { status: 400 });
-};
+import { http } from 'msw';
+import { postSignInResolver, getAccomodationsResolver } from './sign-in';
+
 export const handlers = [
   http.post('/api/auth/owner/signin', postSignInResolver),
   http.get('/api/accommodations/backoffice', getAccomodationsResolver),
