3aa003c3d5bfb80e8eccfd0d24a6320fa58d4f99
89882 <pgh123166@naver.com>
2024-01-07 13:29:15 +0900

    Feat: msw revenue resolver 구현

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,6 +1,7 @@
 import { http } from 'msw';
-import { getStaticsResolver } from './coupon';
+import { getRevenueResolver, getStaticsResolver } from './coupon';
 
 export const handlers = [
   http.get('/api/coupons/backoffice/statistics', getStaticsResolver),
+  http.get('/api/coupons/backoffice/revenue', getRevenueResolver),
 ];
