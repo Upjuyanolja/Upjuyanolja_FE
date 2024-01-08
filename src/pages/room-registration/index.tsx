3ceb23490ed9724c79749b00eb3a3a9e3da353c1
Diana Seo <syb0127@berkeley.edu>
2024-01-08 14:27:13 +0900

    Chore:코드 정리

--- src/pages/room-registration/index.tsx
+++ src/pages/room-registration/index.tsx
@@ -1,99 +1,56 @@
-import { Input, Button, Form, Select, Checkbox, Upload } from 'antd';
-import { UploadOutlined } from '@ant-design/icons';
-import styled from 'styled-components';
 import { colors } from '@/constants/colors';
+import { styled } from 'styled-components';
+import { AccommodationCategory } from '@components/init/init-accommodation-registration/AccommodationCategory';
+import { AccommodationAddress } from '@components/init/init-accommodation-registration/AccommodationAddress';
+import { AccommodationDesc } from '@components/init/init-accommodation-registration/AccommodationDesc';
+import { Form } from 'antd';
+import { ButtonContainer } from '@components/init/ButtonContainer';
+import { CheckBoxContainer } from '@components/init/CheckBoxContainer';
+import { ImageUploadContainer } from '@components/init/ImageUploadContainer';
+import { NameContainer } from '@components/init/NameContainer';
 
 const RoomRegistration = () => {
+  const isValid = true;
+
+  const accommodationOptions = [
+    '객실취사',
+    '주차시설',
+    '픽업 서비스',
+    '바베큐장',
+    '휘트니스센터',
+    '노래방',
+    '에어컨',
+    '사우나실',
+    '세미나실',
+  ];
+
   return (
-    <StyledPageContainer>
-      <StyledForm layout="vertical">
-        <FormTitle>객실 추가 등록</FormTitle>
-        <FormItem label="객실명" name="roomName" rules={[{ required: true }]}>
-          <Input placeholder="객실명을 입력하세요. (ex. 디럭스 더블 룸)" />
-        </FormItem>
-        <FormItem
-          label="객실 가격"
-          name="roomPrice"
-          rules={[{ required: true }]}
-        >
-          <Input addonAfter="원" type="number" />
-        </FormItem>
-        <FormItem label="수소 대표 이미지 설정">
-          <RoomImageUpload
-            listType="picture-card"
-            beforeUpload={() => false} // Implement file handling logic
-          >
-            <div>
-              <UploadOutlined />
-              <div style={{ marginTop: 8 }}>Upload</div>
-            </div>
-          </RoomImageUpload>
-        </FormItem>
-        <FormItem label="객실 수" name="roomQuantity">
-          <Input type="number" min={0} addonAfter="개" />
-        </FormItem>
-        <FormItem label="시간">
-          <Select style={{ width: 100 }}>
-            <Option value="00">00시</Option>
-            {/* Add more options here */}
-          </Select>
-          <Select style={{ width: 100 }}>
-            <Option value="00">00분</Option>
-            {/* Add more options here */}
-          </Select>
-        </FormItem>
-        <FormItem label="인원">
-          <Input type="number" min={0} addonAfter="명" />
-        </FormItem>
-        <FormItem label="객실 옵션">
-          <RoomCheckboxGroup options={['TV', '에어컨', '인터넷']} />
-        </FormItem>
-        <FormItem>
-          <SubmitButton type="primary">등록 완료</SubmitButton>
-        </FormItem>
-      </StyledForm>
-    </StyledPageContainer>
+    <StyledWrapper color={colors.white}>
+      <AccommodationCategory />
+      <Form>
+        <NameContainer labelText="숙소명" />
+        <AccommodationAddress />
+        <ImageUploadContainer />
+        <StyledInputWrapper>
+          <CheckBoxContainer options={accommodationOptions} label="숙소 옵션" />
+        </StyledInputWrapper>
+        <AccommodationDesc />
+        <ButtonContainer buttonStyle={'navigate'} isValid={isValid} />
+      </Form>
+    </StyledWrapper>
   );
 };
 
 export default RoomRegistration;
 
-const { Option } = Select;
+const StyledWrapper = styled.div`
+  background-color: ${(props) => props.color};
 
-const StyledPageContainer = styled.div`
-  padding: 24px;
-  background: ${colors.midGray};
+  padding: 40px;
+
+  border-radius: 8px;
 `;
 
-const StyledForm = styled(Form)`
-  max-width: 700px;
-  margin: auto;
-`;
-
-const FormTitle = styled.h2`
-  font-size: 24px;
-  text-align: left;
-  margin-bottom: 24px;
-`;
-
-const FormItem = styled(Form.Item)`
-  margin-bottom: 16px;
-`;
-
-const SubmitButton = styled(Button)`
-  width: 100%;
-`;
-
-const RoomImageUpload = styled(Upload)`
-  .ant-upload {
-    width: 128px;
-    height: 128px;
-    img {
-      width: 100%;
-    }
-  }
-`;
-
-const RoomCheckboxGroup = styled(Checkbox.Group)`
-  width: 100%;
+const StyledInputWrapper = styled.div`
+  margin-bottom: 48px;
 `;
