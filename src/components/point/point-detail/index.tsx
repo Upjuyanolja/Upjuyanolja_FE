9d683804e68b47111d95d64c52374f8f40123bf5
YongYong21 <imkinggosu@gmail.com>
2024-01-08 18:55:01 +0900

    Style: Point Detail List

---
+++ src/components/point/point-detail/index.tsx
@@ -0,0 +1,11 @@
+import { PointMenu } from './point-detail-menu';
+import { PointDetailList } from './point-detail-list';
+
+export const PointDetailComp = () => {
+  return (
+    <>
+      <PointMenu />
+      <PointDetailList />
+    </>
+  );
+};
