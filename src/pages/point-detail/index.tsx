0d9810a698425fbb8c9a981bb777e3acc93e50b6
YongYong21 <imkinggosu@gmail.com>
2024-01-08 15:29:46 +0900

    Chore: 필요없는 import, 태그 삭제

--- src/pages/point-detail/index.tsx
+++ src/pages/point-detail/index.tsx
@@ -1,32 +1,18 @@
-import { CaretLeftOutlined, CaretRightOutlined } from '@ant-design/icons';
-import { Button, Layout, Space } from 'antd';
-import { TextBox } from '@components/text-box';
+import { Layout } from 'antd';
+
 import styled from 'styled-components';
-import { colors } from '@/constants/colors';
+
 import { PointBox } from '@components/point/point-box';
+import { PointDetailComp } from '@components/point/point-detail';
 
 export const PointDetail = () => {
   return (
     <StyledLayout>
       <PointBox />
-      <Space>
-        <MenuButtonWrap>
-          <li>
-            <Button>전체</Button>
-          </li>
-          <li>
-            <Button>충전</Button>
-          </li>
-          <li>
-            <Button>사용</Button>
-          </li>
-        </MenuButtonWrap>
-      </Space>
+      <PointDetailComp />
     </StyledLayout>
   );
 };
 const StyledLayout = styled(Layout)`
   padding: 32px 48px;
 `;
-
-const MenuButtonWrap = styled('ul')``;
