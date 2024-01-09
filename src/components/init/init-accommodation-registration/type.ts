536a313e5a7555d2d6b077e40f57ee3281b3309f
xxxjinn <tlsguswls319@naver.com>
2024-01-09 12:46:05 +0900

    Feat: recoil state type 지정

--- src/components/init/init-accommodation-registration/type.ts
+++ src/components/init/init-accommodation-registration/type.ts
@@ -39,3 +39,49 @@ export type AddressFormatProps = {
   buildingName: string;
   zonecode: string;
 };
+
+/**request 타입 */
+
+type Image = {
+  url: string;
+};
+
+type Options = {
+  cooking: boolean;
+  parking: boolean;
+  pickup: boolean;
+  barbecue: boolean;
+  fitness: boolean;
+  karaoke: boolean;
+  sauna: boolean;
+  sports: boolean;
+  seminar: boolean;
+};
+
+type Room = {
+  name: string;
+  price: number | null;
+  defaultCapacity: number | null;
+  maxCapacity: number | null;
+  checkInTime: string;
+  checkOutTime: string;
+  count: number | null;
+  images: Image[];
+  options: {
+    airCondition: boolean;
+    tv: boolean;
+    internet: boolean;
+  };
+};
+
+export type UserInputValue = {
+  id: number | null;
+  name: string;
+  address: string;
+  detailAddress: string;
+  description: string;
+  type: string;
+  images: Image[];
+  options: Options;
+  rooms: Room[];
+};
