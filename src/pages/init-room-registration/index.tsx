6bc9afeb1a09114cbd96a593f1ce70dd473df770
xxxjinn <tlsguswls319@naver.com>
2024-01-11 10:11:37 +0900

    Style: 최초 객실 등록 기초 틀

--- src/pages/init-room-registration/index.tsx
+++ src/pages/init-room-registration/index.tsx
@@ -1,3 +1,47 @@
+import { colors } from '@/constants/colors';
+import { ButtonContainer } from '@components/init/ButtonContainer';
+import { CheckBoxContainer } from '@components/init/CheckBoxContainer';
+import { ImageUploadContainer } from '@components/init/ImageUploadContainer';
+import { NameContainer } from '@components/init/NameContainer';
+import { PriceContainer } from '@components/room/price-container';
+import { Form } from 'antd';
+import styled from 'styled-components';
+
 export const InitRoomRegistration = () => {
-  return <div>init room registration page</div>;
+  const [form] = Form.useForm();
+  const isValid = true;
+
+  const roomOptions = ['TV', '에어컨', '인터넷'];
+
+  return (
+    <StyledWrapper>
+      <Form form={form}>
+        <NameContainer header="객실명" />
+        <PriceContainer header="객실 가격" />
+        <ImageUploadContainer header="객실 사진" />
+        <div>
+          <h1>객실수</h1>
+          <div>내용</div>
+        </div>
+        <div>
+          <h1>시간</h1>
+          <div>내용</div>
+        </div>
+        <div>
+          <h1>인원</h1>
+          <div>내용</div>
+        </div>
+        <CheckBoxContainer options={roomOptions} header="객실 옵션" />
+        <ButtonContainer buttonStyle={'navigate'} isValid={isValid} />
+      </Form>
+    </StyledWrapper>
+  );
 };
+
+const StyledWrapper = styled.div`
+  background-color: ${colors.white};
+
+  padding: 40px;
+
+  border-radius: 8px;
+`;
