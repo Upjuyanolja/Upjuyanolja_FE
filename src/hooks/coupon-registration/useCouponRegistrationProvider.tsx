fc43d8dfa35e92455b27f592de7973671cb3344d
Taegwan Youn <tkyoun0421@naver.com>
2024-01-11 12:32:23 +0900

    Chore: 브랜치 최신화

--- src/hooks/coupon-registration/useCouponRegistrationProvider.tsx
+++ src/hooks/coupon-registration/useCouponRegistrationProvider.tsx
@@ -4,18 +4,9 @@ import {
   DISCOUNT_PRICE_NUM,
   DISCOUNT_RATE,
   DISCOUNT_RATE_NUM,
-<<<<<<< HEAD
 } from '@/constants/coupon';
 import { numberFormat, removeNumberFormat } from '@/utils/Format/numberFormat';
 import { useEffect, useState } from 'react';
-=======
-  DISCOUNT_VALUE_INIT,
-} from '@/constants/coupon';
-import { ROUTES } from '@/constants/routes';
-import { numberFormat, removeNumberFormat } from '@/utils/Format/numberFormat';
-import { useEffect, useState } from 'react';
-import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
->>>>>>> a674f3cb3d4dccd1d56dfbd3d759544504336903
 
 export const useCouponRegistrationProvider = () => {
   const [selectedDiscountType, setSelectedDiscountType] =
@@ -23,32 +14,16 @@ export const useCouponRegistrationProvider = () => {
   const [errorMessage, setErrorMessage] = useState('');
   const [discountValue, setDiscountValue] = useState<string>('');
   const [isValidDiscountRange, setIsValidDiscountRange] = useState(true);
-<<<<<<< HEAD
 
   const IS_DISCOUNT_PRICE_TYPE = selectedDiscountType === DISCOUNT_PRICE;
   const IS_DISCOUNT_RATE_TYPE = selectedDiscountType === DISCOUNT_RATE;
 
   // 할인 쿠폰 타입 선택
   useEffect(() => {
-=======
-  const navigate = useNavigate();
-  const { percent } = useParams();
-  const [searchParams] = useSearchParams();
-
-  // 할인 쿠폰 타입 선택
-  useEffect(() => {
-    if (selectedDiscountType === DISCOUNT_PRICE) {
-      navigate(ROUTES.COUPON_REGISTRATION);
-    }
-    if (selectedDiscountType === DISCOUNT_RATE) {
-      navigate(`${ROUTES.COUPON_REGISTRATION}/percent`);
-    }
->>>>>>> a674f3cb3d4dccd1d56dfbd3d759544504336903
     setDiscountValue('');
     setErrorMessage('');
   }, [selectedDiscountType]);
 
-<<<<<<< HEAD
   // 할인가 변경
   useEffect(() => {
     if (!discountValue) {
@@ -64,55 +39,11 @@ export const useCouponRegistrationProvider = () => {
     }
   }, [discountValue]);
 
-=======
-  // searchParams
-  useEffect(() => {
-    if (searchParams.size === DISCOUNT_VALUE_INIT) {
-      return;
-    }
-
-    if (percent) {
-      handleDiscountRateErrorMessage(discountValue);
-    } else {
-      handleDiscountPriceErrorMessage(discountValue);
-    }
-
-    const searchParamsValue = searchParams.get('discount') || '';
-    setDiscountValue(searchParamsValue);
-  }, [searchParams]);
-
-  // 할인가 변경
-  useEffect(() => {
-    if (!discountValue) {
-      setIsValidDiscountRange(true);
-      return;
-    }
-
-    if (percent) {
-      checkDiscountRateValidity(discountValue);
-    } else {
-      checkDiscountPriceValidity(discountValue);
-    }
-  }, [discountValue]);
-
-  // 할인가 변경 시 parameter로 할인가 정보 넣는 함수
-  const handleNavigate = (discountValue: string) => {
-    if (percent) {
-      navigate(
-        `${ROUTES.COUPON_REGISTRATION}/percent?discount=${discountValue}`,
-      );
-    } else {
-      navigate(`${ROUTES.COUPON_REGISTRATION}?discount=${discountValue}`);
-    }
-  };
-
->>>>>>> a674f3cb3d4dccd1d56dfbd3d759544504336903
   // input blur 시 실행되는 함수
   const handleBlur = async (discountValue: string) => {
     if (!discountValue) {
       return;
     }
-<<<<<<< HEAD
 
     if (IS_DISCOUNT_PRICE_TYPE) {
       await handleDiscountPriceErrorMessage(discountValue);
@@ -123,9 +54,6 @@ export const useCouponRegistrationProvider = () => {
       await handleDiscountRateErrorMessage(discountValue);
       await checkDiscountRateValidity(discountValue);
     }
-=======
-    await handleNavigate(discountValue);
->>>>>>> a674f3cb3d4dccd1d56dfbd3d759544504336903
     const formattedValue = numberFormat(discountValue);
     await setDiscountValue(formattedValue);
   };
@@ -189,10 +117,6 @@ export const useCouponRegistrationProvider = () => {
     DISCOUNT_PRICE,
     DISCOUNT_RATE,
     selectedDiscountType,
-<<<<<<< HEAD
-=======
-    handleNavigate,
->>>>>>> a674f3cb3d4dccd1d56dfbd3d759544504336903
     errorMessage,
     discountValue,
     setDiscountValue,
