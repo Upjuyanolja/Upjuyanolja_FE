d2c2f907ffbf4b04294c66394abc17332885c0a6
YongYong21 <imkinggosu@gmail.com>
2024-01-12 16:47:06 +0900

    Feat: point-detail 훅, 리액트 쿼리 추가

---
+++ src/hooks/point-detail/index.tsx
@@ -0,0 +1,33 @@
+import { menuStatusType } from '@api/point-detail/get-point-detail/type';
+import { useGetPointSummary } from '@queries/point';
+import { useGetPointDetail } from '@queries/point-detail';
+export const usePointDetail = (menuStatus: menuStatusType, page: number) => {
+  const {
+    data: pointSummaryData,
+    isError: isPointSummaryError,
+    refetch: pointSummaryDataRefetch,
+  } = useGetPointSummary({
+    select(data) {
+      return data.data.data;
+    },
+    staleTime: 60 * 60 * 1000,
+  });
+
+  const {
+    data: pointDetailData,
+    isError: isPointDetailError,
+    refetch: pointDetailDataRefetch,
+  } = useGetPointDetail(menuStatus, page, {
+    select(data) {
+      return data.data.data;
+    },
+  });
+  return {
+    pointDetailData,
+    isPointDetailError,
+    pointSummaryDataRefetch,
+    pointSummaryData,
+    isPointSummaryError,
+    pointDetailDataRefetch,
+  };
+};
