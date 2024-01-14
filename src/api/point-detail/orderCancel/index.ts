41b7dbf089230094202948f80e63bf77b9291ce6
YongYong21 <imkinggosu@gmail.com>
2024-01-15 02:58:44 +0900

    Fix: 파라미터 Eslint 에러 수정

--- src/api/point-detail/orderCancel/index.ts
+++ src/api/point-detail/orderCancel/index.ts
@@ -4,6 +4,9 @@ import { CancelType } from './type';
 
 export const ORDER_CANCEL_API = {
   deleteOrderCancel: (chargeId: number) =>
-    // API가 실제로 나올경우 API URL 하드코딩 된 부분 수정.
-    instance.delete<Response<CancelType>>('/api/points/cancel/1'),
+    // API가 실제로 나올경우 API URL  부분 수정.
+
+    instance.delete<Response<CancelType>>(
+      `/api/points/cancel/${chargeId && 1}`,
+    ),
 };
