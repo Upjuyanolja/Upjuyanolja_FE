62ee9338ad6bace5af82542afed3ff6853c62b18
89882 <pgh123166@naver.com>
2024-01-15 00:45:57 +0900

    Feat: 쿠폰 수정 api 구현

--- src/api/coupon/index.ts
+++ src/api/coupon/index.ts
@@ -1,6 +1,12 @@
 import { Response } from '@/types/api';
 import { instance } from '..';
-import { CouponDeleteParams, coupons, revenueData, staticsData } from './type';
+import {
+  CouponDeleteParams,
+  CouponEditParams,
+  coupons,
+  revenueData,
+  staticsData,
+} from './type';
 
 export const COUPON_API = {
   getStatics: () =>
@@ -13,4 +19,6 @@ export const COUPON_API = {
     instance.get<Response<null>>('/api/coupons/backoffice/manage', {
       data: params,
     }),
+  editCoupon: (params: CouponEditParams) =>
+    instance.patch<Response<null>>('/api/coupons/backoffice/manage', params),
 };
