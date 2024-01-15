806f22114055c68a3723e01dc42cd42a92280c8c
xxxjinn <tlsguswls319@naver.com>
2024-01-15 10:53:14 +0900

    Refactor: 인원/객실수 컴포넌트에서 form 에러 안나도록 설정 (defaultValue -> initialValue)

--- src/components/room/num-of-rooms-container/index.tsx
+++ src/components/room/num-of-rooms-container/index.tsx
@@ -47,11 +47,10 @@ export const CountContainer = ({ header, form }: NumOfRoomsContainerProps) => {
             일일 예약 가능 객실
           </TextBox>
         </StyledTextBoxWrapper>
-        <Form.Item name={'count'} style={{}}>
+        <Form.Item name={'count'} initialValue={1}>
           <StyledInputNumber
             min={1}
             max={100}
-            defaultValue={1}
             value={numOfRooms}
             onChange={handleCapacityChange}
           />
