8f509bc669788d08698bb8fc2e58407e71f82b84
xxxjinn <tlsguswls319@naver.com>
2024-01-16 18:19:55 +0900

    Style: 숙소 소개 div style 변경 (height -> max-height)

--- src/components/init/init-info-confirmation/AccommodationInfo.tsx
+++ src/components/init/init-info-confirmation/AccommodationInfo.tsx
@@ -169,7 +169,7 @@ const StyledWrapper = styled.div`
 `;
 
 const StyledAccommodationInfoContainer = styled.div`
-  height: 360px;
+  max-height: 360px;
 
   border-radius: 8px;
   border: 2px solid ${colors.primary};
