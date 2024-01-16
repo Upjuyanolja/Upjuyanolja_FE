95182f8a50ab71b3961c505f1e3ad1a483fbcf85
xxxjinn <tlsguswls319@naver.com>
2024-01-16 18:13:02 +0900

    Refactor: localStorage key값 수정

--- src/stores/init/atoms.ts
+++ src/stores/init/atoms.ts
@@ -7,7 +7,7 @@ import { atom } from 'recoil';
 import { recoilPersist } from 'recoil-persist';
 
 const { persistAtom } = recoilPersist({
-  key: 'localStorage',
+  key: 'userInput',
   storage: localStorage,
 });
 
