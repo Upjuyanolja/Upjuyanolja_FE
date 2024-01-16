597ce9d454bf3a817f0b85579d0810ab54efc560
YongYong21 <imkinggosu@gmail.com>
2024-01-16 14:53:15 +0900

    Chore: 콘솔 삭제.

--- src/pages/toss-success/index.tsx
+++ src/pages/toss-success/index.tsx
@@ -25,7 +25,6 @@ export const TossSuccess = () => {
         paymentKey,
         amount: parseInt(amount),
       };
-      console.log(data);
       pointChargeMutation.mutateAsync(data).then(() => {
         message.success({
           content: (
@@ -39,7 +38,6 @@ export const TossSuccess = () => {
       });
     }
   }, []);
-  //결제 성공시 파라미터 URL point-detail?paymentType=NsORMAL&orderId=zc0hRbNHRA6sL2Z2BGXbA&paymentKey=gN60L1adJYyZqmkKeP8gxYMjeX2DZp3bQRxB9lG5DnzWE7pM&amount=1000
-  //http://localhost:3000/toss-success?paymentType=NORMAL&orderId=yLFv9nP9MZE8W8Hu_rXfZ&paymentKey=a90ZoyegEOALnQvDd2VJojv7mWpWoPrMj7X41mNW5kzKbwG6&amount=10000
+
   return <div></div>;
 };
