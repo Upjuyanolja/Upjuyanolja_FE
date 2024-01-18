ef335608b3b59ed8ec9e40d1acdbfecbb15e48d5
xxxjinn <tlsguswls319@naver.com>
2024-01-19 03:00:10 +0900

    Refactor: routes 상수 사용하지 않았던 부분 수정

--- src/pages/init/index.tsx
+++ src/pages/init/index.tsx
@@ -1,4 +1,5 @@
 import { colors } from '@/constants/colors';
+import { ROUTES } from '@/constants/routes';
 import { TextBox } from '@components/text-box';
 import { Layout, Button } from 'antd';
 import { MdOutlineAddHome } from 'react-icons/md';
@@ -19,7 +20,7 @@ export const Init = () => {
           <StyledButton
             ghost
             type="primary"
-            href="/init/accommodation-registration"
+            href={ROUTES.INIT_ACCOMMODATION_REGISTRATION}
           >
             <MdOutlineAddHome />
             숙소 등록 시작하기
