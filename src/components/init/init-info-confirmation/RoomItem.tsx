6e47c8639886277c8f4ffaa1c91d0be436a590eb
xxxjinn <tlsguswls319@naver.com>
2024-01-18 20:46:12 +0900

    Style: message 위치 수정

--- src/components/init/init-info-confirmation/RoomItem.tsx
+++ src/components/init/init-info-confirmation/RoomItem.tsx
@@ -16,7 +16,10 @@ export const RoomItem = () => {
 
   const removeRoom = (room: Room) => {
     if (userInputValue[0].rooms.length === 1) {
-      message.error('최소 1개의 객실이 등록되어야 합니다.');
+      message.error({
+        content: '최소 1개의 객실이 등록되어야 합니다.',
+        style: { marginTop: '210px' },
+      });
       return;
     }
 
@@ -35,7 +38,10 @@ export const RoomItem = () => {
       return [updatedUserInputValue];
     });
 
-    message.success('삭제되었습니다.');
+    message.success({
+      content: '삭제되었습니다.',
+      style: { marginTop: '210px' },
+    });
   };
 
   const confirm = (room: Room) => {
