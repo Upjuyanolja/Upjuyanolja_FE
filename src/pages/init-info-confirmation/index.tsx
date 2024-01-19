18167046fa915a22a002a0172a7ed53be2d52e38
xxxjinn <tlsguswls319@naver.com>
2024-01-19 20:46:57 +0900

    Refactor: !userInput으로 수정

--- src/pages/init-info-confirmation/index.tsx
+++ src/pages/init-info-confirmation/index.tsx
@@ -11,7 +11,7 @@ import styled from 'styled-components';
 export const InitInfoConfirmation = () => {
   const userInput = window.localStorage.getItem('userInput');
   const navigate = useNavigate();
-  if (userInput === null) {
+  if (!userInput) {
     return (
       <StyledWrapper>
         <StyledNoInputWrapper>
