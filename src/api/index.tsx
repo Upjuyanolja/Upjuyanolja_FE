fe1c7227bcb52371a5d40390d3b4ce8debb629b9
yangjaehyuk <gndksakxk10@naver.com>
2024-01-21 15:09:06 +0900

    Fix: interceptor 수정

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -3,11 +3,13 @@ import { HTTP_BASE_URL, HTTP_STATUS_CODE } from '../constants/api';
 import { getCookie, removeCookie, setCookie } from '@hooks/sign-in/useSignIn';
 import { message } from 'antd';
 import { TextBox } from '@components/text-box';
+import { ROUTES } from '@/constants/routes';
 
 export const instance = axios.create({
   baseURL: HTTP_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
+    timeout: 5000,
   },
 });
 
@@ -33,53 +35,50 @@ instance.interceptors.response.use(
     return response;
   },
   async (error) => {
-    if (
-      window.location.pathname !== '/signin' &&
-      error?.response.status === HTTP_STATUS_CODE.UNAUTHORIZED
+    const accessToken = getCookie('accessToken');
+    if (window.location.pathname !== ROUTES.SIGNIN && !accessToken) {
+      message.error({
+        content: (
+          <TextBox typography="body3" fontWeight={'400'}>
+            accessToken이 없습니다.
+          </TextBox>
+        ),
+        duration: 2,
+      });
+      removeCookie('accessToken');
+      removeCookie('refreshToken');
+      removeCookie('accommodationId');
+      setTimeout(() => {
+        window.location.href = ROUTES.SIGNIN;
+      }, 1000);
+      return Promise.reject(error);
+    } else if (
+      window.location.pathname !== ROUTES.SIGNIN &&
+      error.response.status === HTTP_STATUS_CODE.UNAUTHORIZED
     ) {
-      const refreshToken = getCookie('refreshToken');
-      if (refreshToken) {
-        try {
-          // 여기에 재발급 api 선언, 아래는 예시
-          const newAccessToken = 'ivegaeul';
-          setCookie('accessToken', newAccessToken);
-          return axios(error.config);
-        } catch (refreshError) {
-          removeCookie('accessToken');
-          removeCookie('refreshToken');
-          removeCookie('accommodationId');
-          message.error({
-            content: (
-              <TextBox typography="body3" fontWeight={'400'}>
-                refreshToken이 올바르지 않습니다.
-              </TextBox>
-            ),
-            duration: 2,
-          });
-          setTimeout(() => {
-            window.location.href = '/signin';
-          }, 1000);
-          return Promise.reject(refreshError);
-        }
-      } else {
+      try {
+        // 여기에 재발급 api 선언, 아래는 예시
+        const newAccessToken = 'ivegaeul';
+        setCookie('accessToken', newAccessToken);
+        return axios(error.config);
+      } catch (refreshError) {
         removeCookie('accessToken');
         removeCookie('refreshToken');
         removeCookie('accommodationId');
         message.error({
           content: (
             <TextBox typography="body3" fontWeight={'400'}>
-              로그인 만료 입니다.
+              로그인 만료입니다.
             </TextBox>
           ),
           duration: 2,
         });
         setTimeout(() => {
-          window.location.href = '/signin';
+          window.location.href = ROUTES.SIGNIN;
         }, 1000);
-        return Promise.reject(error);
+        return Promise.reject(refreshError);
       }
     }
-    return Promise.reject(error);
   },
 );
 
