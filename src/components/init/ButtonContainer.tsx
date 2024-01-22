d707932ee18ed0168ffe87b414409c6eaaebc0bd
xxxjinn <tlsguswls319@naver.com>
2024-01-22 16:36:13 +0900

    Fix: 숙소 등록 후 userInputValue images 초기화 수정

--- src/components/init/ButtonContainer.tsx
+++ src/components/init/ButtonContainer.tsx
@@ -90,7 +90,7 @@ export const ButtonContainer = ({
         zipCode: '',
         description: '',
         type: '',
-        images: [{ url: '' }],
+        images: [],
         options: {
           cooking: false,
           parking: false,
