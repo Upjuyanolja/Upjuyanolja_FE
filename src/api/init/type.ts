8bfe6dd59b3baeac335ca4873e35b5f78a10ce68
xxxjinn <tlsguswls319@naver.com>
2024-01-22 16:16:32 +0900

    Refactor: postAccommodation type 수정

--- src/api/init/type.ts
+++ src/api/init/type.ts
@@ -39,40 +39,36 @@ export type PostAccommodationParams = {
 };
 
 export type PostAccommodation = {
-  message: string;
-  code?: number | undefined;
-  data: {
-    accommodationId: number;
-    name: string;
-    address: string;
-    description: string;
-    type: string;
-    images: { url: string }[];
-    options: {
-      cooking: boolean;
-      parking: boolean;
-      pickup: boolean;
-      barbecue: boolean;
-      fitness: boolean;
-      karaoke: boolean;
-      sauna: boolean;
-      sports: boolean;
-      seminar: boolean;
-    };
-    rooms: {
-      name: string;
-      price: number;
-      defaultCapacity: number;
-      maxCapacity: number;
-      checkInTime: string;
-      checkOutTime: string;
-      count: number;
-      images: { id: number; url: string }[];
-      options: {
-        airCondition: boolean;
-        tv: boolean;
-        internet: boolean;
-      };
-    }[];
+  accommodationId: number;
+  name: string;
+  address: string;
+  description: string;
+  type: string;
+  images: { url: string }[];
+  options: {
+    cooking: boolean;
+    parking: boolean;
+    pickup: boolean;
+    barbecue: boolean;
+    fitness: boolean;
+    karaoke: boolean;
+    sauna: boolean;
+    sports: boolean;
+    seminar: boolean;
   };
+  rooms: {
+    name: string;
+    price: number;
+    defaultCapacity: number;
+    maxCapacity: number;
+    checkInTime: string;
+    checkOutTime: string;
+    count: number;
+    images: { id: number; url: string }[];
+    options: {
+      airCondition: boolean;
+      tv: boolean;
+      internet: boolean;
+    };
+  }[];
 };
