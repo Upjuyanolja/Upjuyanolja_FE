5999b8fad223cc19647b91d341ab5b2289bb9630
YongYong21 <imkinggosu@gmail.com>
2024-01-22 11:47:25 +0900

    Update atoms.ts

--- src/stores/point-detail/atoms.ts
+++ src/stores/point-detail/atoms.ts
@@ -42,38 +42,38 @@ export const pointSummaryDataState = atom<pointSummaryData>({
 export const pointDetailDataState = atom<PointDetailDataType>({
   key: 'pointDetailDataState',
   default: {
-    pageNum: 1,
-    pageSize: 4,
-    totalPages: 10,
-    totalElements: 40,
+    pageNum: -1,
+    pageSize: -1,
+    totalPages: -1,
+    totalElements: -1,
     isLast: true,
     histories: [
       {
-        id: 1,
-        category: '충전',
-        type: '포인트',
+        id: -1,
+        category: '',
+        type: '',
         status: '구매 확정',
-        name: '포인트 충전',
+        name: '',
         description: '',
-        trade: 5000,
-        amount: 5000,
+        trade: -1,
+        amount: -1,
         receipt: {
-          orderId: 'O-1644460169123',
-          tradeAt: '2023.12.20 18:06',
-          amount: '5000',
+          orderId: '',
+          tradeAt: '',
+          amount: '',
           orders: [
             {
-              room: '스탠다드 트윈 룸',
+              room: '',
               coupons: [
                 {
-                  name: '1,000원 쿠폰 | 100P',
-                  count: 2,
-                  totalPrice: 100,
+                  name: '',
+                  count: -1,
+                  totalPrice: -1,
                 },
                 {
-                  name: '1,000원 쿠폰 | 100P',
-                  count: 1,
-                  totalPrice: 50,
+                  name: '',
+                  count: -1,
+                  totalPrice: -1,
                 },
               ],
             },
