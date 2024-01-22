a0345b2102c91d7f56d0044cada25f1ccf209a90
YongYong21 <imkinggosu@gmail.com>
2024-01-22 11:07:18 +0900

    Chore: interface -> type 수정

--- src/api/point-detail/get-point-detail/type.ts
+++ src/api/point-detail/get-point-detail/type.ts
@@ -1,25 +1,25 @@
 import { badgeProps } from '@components/point-detail/badge/types';
 
-export interface Coupons {
+export type Coupons = {
   name: string;
   count: number;
   totalPrice: number;
-}
+};
 
-export interface Orders {
+export type Orders = {
   room: string;
   coupons: Coupons[];
-}
+};
 
-export interface Receipt {
+export type Receipt = {
   orderId: string;
   tradeAt: string;
   amount: string;
   accommodationName?: string;
   orders?: Orders[];
-}
+};
 
-export interface History {
+export type History = {
   id: number;
   category: string;
   type: string;
@@ -29,15 +29,15 @@ export interface History {
   trade: number;
   amount: number;
   receipt: Receipt;
-}
+};
 
-export interface PointDetailDataType {
+export type PointDetailDataType = {
   pageNum: number;
   pageSize: number;
   totalPages: number;
   totalElements: number;
   isLast: boolean;
   histories: History[];
-}
+};
 
 export type menuStatusType = 'total' | 'charges' | 'usage';
