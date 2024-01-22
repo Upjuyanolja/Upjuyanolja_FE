b4435b527070d9ffe863a971b67f884e5a489410
YongYong21 <imkinggosu@gmail.com>
2024-01-22 10:26:51 +0900

    Update: API 수정에 따른 타입 추가

--- src/api/point-detail/get-point-detail/type.ts
+++ src/api/point-detail/get-point-detail/type.ts
@@ -1,9 +1,22 @@
 import { badgeProps } from '@components/point-detail/badge/types';
 
+export interface Coupons {
+  name: string;
+  count: number;
+  totalPrice: number;
+}
+
+export interface Orders {
+  room: string;
+  coupons: Coupons[];
+}
+
 export interface Receipt {
   orderId: string;
   tradeAt: string;
   amount: string;
+  accommodationName?: string;
+  orders?: Orders[];
 }
 
 export interface History {
@@ -13,7 +26,8 @@ export interface History {
   status: badgeProps;
   name: string;
   description: string;
-  trade: string;
+  trade: number;
+  amount: number;
   receipt: Receipt;
 }
 
