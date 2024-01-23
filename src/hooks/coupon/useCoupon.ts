c482b282ca5cb2706262c0b7abc20f5b1532c608
89882 <pgh123166@naver.com>
2024-01-23 23:08:36 +0900

    Fix: api response 형태 수정

--- src/hooks/coupon/useCoupon.ts
+++ src/hooks/coupon/useCoupon.ts
@@ -66,7 +66,7 @@ export const useCoupon = () => {
     remove: getCouponRemove,
   } = useGetCoupon(accommodationId as string, {
     select(data) {
-      return data.data.data;
+      return data.data;
     },
   });
 
