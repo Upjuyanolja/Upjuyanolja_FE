d1453dad6558fbf4edde75f7b26360dd7bc532d5
YongYong21 <imkinggosu@gmail.com>
2024-01-27 16:55:21 +0900

    Fix: queryClient.invalidateQueries 추가

--- src/queries/point-charge-modal/index.tsx
+++ src/queries/point-charge-modal/index.tsx
@@ -40,7 +40,9 @@ export const usePointCharge = (
     TossRequestType
   >((data: TossRequestType) => POINT_CHARGE_API.postPointCharge(data), {
     onSuccess: () => {
-      queryClient.invalidateQueries(['getPointSummary', 'getPointTotal']);
+      queryClient.invalidateQueries(['getPointSummary']);
+
+      queryClient.invalidateQueries(['getPointTotal']);
       message.success({
         content: '결제가 완료되었습니다.',
         duration: 2,
